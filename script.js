e66ea3c2a19c8139d8b9798c2a2074b508b99638
Sufiyaan Tamboli <sufiyaantamboli1983@gmail.com>
2025-09-10 20:41:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-// Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function() {
+  return this.make + " " + this.model;
+};
+
+function SportsCar(make, model, topSpeed) {
+  Car.call(this, make, model); // Call Car constructor with this context
+  this.topSpeed = topSpeed;
+}
+
+// Inherit from Car.prototype
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Add getTopSpeed to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
